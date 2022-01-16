0b3528da15719db7e46e042f60d60e0de2a74697
DenDy1227 <you@example>
2022-01-16 15:04:51 +0200

    first commit

---
+++ formik HW/src/FormExample.jsx
@@ -0,0 +1,19 @@
+
+// Instruments
+
+
+
+import BasicTabs from './tabs/tabs';
+
+export function AppExample() {
+
+    return <>
+        <BasicTabs/>
+         </>
+             
+        
+}
+
+
+
+{/* <Layout><Registration /> </Layout> */}
---
+++ formik HW/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+
+import { AppExample } from './FormExample';
+
+
+
+ReactDOM.render(
+    <AppExample />,
+  document.getElementById('root')
+);
